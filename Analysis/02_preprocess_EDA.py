b394dfca9835d25459fae9f44bdde5118e9719a4
Sein Jeung <seinjeung@gmail.com>
2025-11-27 12:21:40 +0100

    Covert the signal

--- Analysis/02_preprocess_EDA.py
+++ Analysis/02_preprocess_EDA.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import os
 import neurokit2 as nk
 
-
 dataFolder = r'C:\Users\seinj\Teaching2526\Psychophysiology2526\Data'
 rawFolder = os.path.join(dataFolder, '01_raw-data')
 
@@ -20,7 +19,12 @@ filename = os.path.join(rawFolder, 'raw_demo-' + pts + '_' + tsk + '.txt')
 print('reading in ' + filename)
 
 subdata = pd.read_csv(filename)
-eda_data = subdata["eda"].values
+raw_eda_data = subdata["eda"].values
+
+# Apply transfer function
+VCC = 3.3
+sampling_resolution = 10
+eda_data = (raw_eda_data/2**sampling_resolution)*VCC/0.12
 
 # preprocess
 signals_eda, info_eda = nk.eda_process(eda_data, sampling_rate=1000)
@@ -48,5 +52,5 @@ plt.tight_layout()
 plt.show()
 
 # neurokit summary
-#fig = nk.eda_plot(signals_eda)
-#plt.show()
+fig = nk.eda_plot(signals_eda)
+plt.show()
