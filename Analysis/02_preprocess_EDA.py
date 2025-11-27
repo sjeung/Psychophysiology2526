b46ce8a3fc1e73488c71bb5681510de0ec47adfd
Sein Jeung <seinjeung@gmail.com>
2025-11-27 12:28:23 +0100

    Loop over participants and sessions

--- Analysis/02_preprocess_EDA.py
+++ Analysis/02_preprocess_EDA.py
@@ -10,47 +10,55 @@ resultsFolder = r'C:\Users\seinj\Teaching2526\Psychophysiology2526\Results'
 os.makedirs(resultsFolder, exist_ok=True)
 
 # parameters
-pts = 'sub-001'
-tsk = 'baseline'
+participants = ['sub-001', 'sub-002']
+tasks = ['baseline', 'addition', 'subtract']
 
-# assemble file name
-filename = os.path.join(rawFolder, 'raw_demo-' + pts + '_' + tsk + '.txt')
+for pts in participants:
+    for tsk in tasks:
+        # assemble file name
+        filename = os.path.join(rawFolder, 'raw_demo-' + pts + '_' + tsk + '.txt')
 
-print('reading in ' + filename)
+        print('reading in ' + filename)
 
-subdata = pd.read_csv(filename)
-raw_eda_data = subdata["eda"].values
+        subdata = pd.read_csv(filename)
+        raw_eda_data = subdata["eda"].values
 
-# Apply transfer function
-VCC = 3.3
-sampling_resolution = 10
-eda_data = (raw_eda_data/2**sampling_resolution)*VCC/0.12
+        # Apply transfer function
+        VCC = 3.3
+        sampling_resolution = 10
+        eda_data = (raw_eda_data/2**sampling_resolution)*VCC/0.12
 
-# preprocess
-signals_eda, info_eda = nk.eda_process(eda_data, sampling_rate=1000)
+        # preprocess
+        signals_eda, info_eda = nk.eda_process(eda_data, sampling_rate=1000)
 
-plt.figure(figsize=(14, 8))
+        plt.figure(figsize=(14, 8))
 
-# 1) Raw EDA
-plt.subplot(2, 1, 1)
-plt.plot(eda_data[1:20000], linewidth=0.8)
-plt.title("Raw EDA Signal (Unprocessed)")
-plt.xlabel("Samples")
-plt.ylabel("Amplitude (µS)")
+        # 1) Raw EDA
+        plt.subplot(2, 1, 1)
+        plt.plot(eda_data[1:20000], linewidth=0.8)
+        plt.title("Raw EDA Signal (Unprocessed)")
+        plt.xlabel("Samples")
+        plt.ylabel("Amplitude (µS)")
 
-# 2) Processed EDA
-plt.subplot(2, 1, 2)
-plt.plot(signals_eda["EDA_Clean"], linewidth=0.8, label="Clean EDA")
-plt.plot(signals_eda["EDA_Tonic"], linewidth=0.8, label="Tonic (SCL)")
-plt.plot(signals_eda["EDA_Phasic"], linewidth=0.8, label="Phasic (SCR)")
-plt.title("Processed EDA (Cleaned, Tonic, Phasic)")
-plt.xlabel("Samples")
-plt.ylabel("Amplitude (µS)")
-plt.legend()
-plt.tight_layout()
-#plt.savefig("...eda_preproc.png")
-plt.show()
+        # 2) Processed EDA
+        plt.subplot(2, 1, 2)
+        plt.plot(signals_eda["EDA_Clean"], linewidth=0.8, label="Clean EDA")
+        plt.plot(signals_eda["EDA_Tonic"], linewidth=0.8, label="Tonic (SCL)")
+        plt.plot(signals_eda["EDA_Phasic"], linewidth=0.8, label="Phasic (SCR)")
+        plt.title("Processed EDA (Cleaned, Tonic, Phasic)")
+        plt.xlabel("Samples")
+        plt.ylabel("Amplitude (µS)")
+        plt.legend()
+        plt.tight_layout()
 
-# neurokit summary
-fig = nk.eda_plot(signals_eda)
-plt.show()
+        # save figure
+        figures_folder = os.path.join(resultsFolder, 'figures')
+        os.makedirs(figures_folder, exist_ok=True)
+        figure_name = os.path.join(figures_folder, 'processed_EDA_' + pts + '_' + tsk + '.png')
+        plt.savefig(figure_name)
+
+        # neurokit summary
+        fig = nk.eda_plot(signals_eda)
+        figure_name = os.path.join(figures_folder, 'nk_summary_EDA_' + pts + '_' + tsk + '.png')
+        plt.savefig(figure_name)
+
