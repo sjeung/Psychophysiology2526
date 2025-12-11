e38481b22a2692199075bd6ed3ed27392d608fd8
Sein Jeung <seinjeung@gmail.com>
2025-12-11 12:11:56 +0100

    Summary EDA results

--- Analysis/03_summarize_EDA.py
+++ Analysis/03_summarize_EDA.py
@@ -17,81 +17,63 @@ print(alldata.keys())
 key_features = ["tonic"]
 
 # subject specific line plot
-for feat in key_features:
-    plt.figure(figsize=(8, 4))
+plt.figure(figsize=(8, 4))
 
-    for subj in alldata["Subject"].unique():
-        sub_df = alldata[alldata["Subject"] == subj]
-        plt.plot(sub_df["Session"], sub_df[feat], marker="o", label=subj)
+for subj in alldata["Subject"].unique():
+    sub_df = alldata[alldata["Subject"] == subj]
+    plt.plot(sub_df["Session"], sub_df["tonic"], marker="o", label=subj)
 
-    plt.title(f"{feat} Across Sessions")
-    plt.xlabel("Session")
-    plt.ylabel(feat)
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
-
+plt.title(f"Tonic EDA Across Sessions")
+plt.xlabel("Session")
+plt.ylabel("Tonic EDA (μS)")
+plt.legend()
+plt.tight_layout()
+plt.show()
 
 # Normalisation
-# Normalized_value = value / subject's_baseline_value
-# So baseline becomes 1.0 for everyone.
+alldata["tonic_Norm,"] = None   # create empty column
 
-#for feature in key_features:
+# Process each subject separately
+for subject_id, subject_data in alldata.groupby("Subject"):
 
- #   norm_col = feature + "_Norm"
- #   alldata[norm_col] = None   # create empty column
+    # Find the subject's baseline value
+    baseline_value = subject_data.loc[
+     subject_data["Session"] == "baseline", "tonic"
+    ].values[0]
 
-    # Process each subject separately
- #   for subject_id, subject_data in alldata.groupby("Subject"):
+    # Compute normalized values for ALL that subject's sessions
+    normalized_values = subject_data["tonic"]*100 / baseline_value
 
-        # Find the subject's baseline value
-  #      baseline_value = subject_data.loc[
-   #         subject_data["Session"] == "baseline", feature
-   #     ].values[0]
+    # Save back to the main DataFrame
+    alldata.loc[subject_data.index, "tonic_Norm"] = normalized_values
 
-        # Compute normalized values for ALL that subject's sessions
-    #    normalized_values = subject_data[feature] / baseline_value
+# plot
+plot_data = alldata[alldata["Session"].isin(["addition", "subtract"])]
+plt.figure(figsize=(7, 4))
+plt.title("Normalized tonic EDA")
+plt.xlabel("Session")
+plt.ylabel("Normalized SCL in %")
 
-        # Save back to the main DataFrame
-     #   alldata.loc[subject_data.index, norm_col] = normalized_values
+# Bar plot (mean per session)
+sns.barplot(
+    data=plot_data,
+    x="Session",
+    y="tonic_Norm",
+    color="lightgray",
+    width=0.3
+)
 
-#print("\nAdded baseline-normalized columns:")
-#print([f + "_Norm" for f in key_features], "\n")
+# Dot plot (each subject)
+sns.stripplot(
+    data=plot_data,
+    x="Session",
+    y="tonic_Norm",
+    hue="Subject",
+    dodge=True,
+    alpha=0.8
+)
 
-
-plot_data = alldata[alldata["Session"].isin(["Addition", "Subtraction"])]
-
-#  BAR PLOTS WITH NORMALIZED VALUES
-for feature in key_features:
-
-    # only select addition / subtract sessions
-    plot_data = alldata[alldata["Session"].isin(["addition", "subtract"])]
-    norm_col = feature + "_Norm"
-
-    plt.figure(figsize=(7, 4))
-    plt.title(f"{feature} by Session (Normalized to Baseline)")
-    plt.xlabel("Session")
-    plt.ylabel("Normalized Value (Baseline = 1.0)")
-
-    # Bar plot (mean per session)
-    sns.barplot(
-        data=plot_data,
-        x="Session",
-        y=norm_col,
-        color="lightgray"
-    )
-
-    # Dot plot (each subject)
-    sns.stripplot(
-        data=plot_data,
-        x="Session",
-        y=norm_col,
-        hue="Subject",
-        dodge=True,
-        alpha=0.8
-    )
-
-    #plt.axhline(1.0, color="black", linestyle="--")
-    plt.legend(title="Subject", bbox_to_anchor=(1.05, 1), loc="upper left")
-    plt.tight_layout()
-    plt.show()
+plt.legend(title="Subject", bbox_to_anchor=(1.05, 1), loc="upper left")
+plt.tight_layout()
+plt.ylim(70,160)
+plt.show()
